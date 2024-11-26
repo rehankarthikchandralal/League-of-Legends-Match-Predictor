4eff119b12e6d3e8d1c14b653a2abc6692fb917c
Rehan <rehankarthik12@gmail.com>
2024-11-26 13:02:26 +0100

    Added in comment lines for better readability

--- src/League_of_Legends_match_Predictor.py
+++ src/League_of_Legends_match_Predictor.py
@@ -6,16 +6,18 @@ from sklearn.preprocessing import StandardScaler
 data = pd.read_csv('/home/rehan/Projects/League_of_Legends_match_Predictor/league_of_legends_data_large.csv')
 
 #  Split data into features (X) and target (y)
+#  win is the target and the remaining columns features  
 X = data.drop('win', axis=1)  
 y = data['win']  
 
-# Split the data into training and testing sets
+#  Split the data into training and testing sets ( 20% for testing and 80% for training)
+#  random_state=42 to ensure reproducibility of the split.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #  Standardize the features
 scaler = StandardScaler()
 
-# Fit the scaler on the training data and transform both training and testing sets
+# Fit the scaler on the training data and transform both training and testing sets into Pytorch tensors
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
